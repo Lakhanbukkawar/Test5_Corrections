2e3ffc7fddeb2be59a8df43be871a8b87307de5b
Lakhanbukkawar <laakhanbukkawar@gmail.com>
2020-09-14 11:09:57 +0530

    test corrections

---
+++ question_10.py
@@ -0,0 +1,2 @@
+list1 = [3,4,5,20,5,25,1,3]
+print(list1.reverse())
---
+++ question_33.py
@@ -0,0 +1,5 @@
+x=1
+print(x<<2)
+#The binary form of 1 is 0001.this is bitwise left shift on the x 
+#this shift gives the value 0100 which is binary form of number 4
+    
